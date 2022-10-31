5280365e3d71d4db5c5bd9a4805e307a4959bbcf
112c <tobyfox2020@gmail.com>
2022-10-31 19:03:24 +0000

    Initial Commit

---
+++ Peony/PeonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeonyApp.swift
+//  Peony
+//
+//  Created by Toby Fox on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
